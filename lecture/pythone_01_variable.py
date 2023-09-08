6b40b93dd48fb9b90a1157dc242f7cafeb60ec98
euimeen <144198069+euimeen@users.noreply.github.com>
2023-09-08 14:47:45 +0900

    Add Python_01_variable

---
+++ lecture/pythone_01_variable.py
@@ -0,0 +1,9 @@
+# 주석(comment): 메모, 실행x
+#  글씨체: D2Coding
+# 1.출력 함수(print)
+# - ()안의 값을 출력
+# - 변수 값 확인 용도로 많이 사용
+    print("Hello Python")
+# 문자열 타입(string type)
+#- python: ''or "" -> string
+# - c, java: ''(char), ""(string)
